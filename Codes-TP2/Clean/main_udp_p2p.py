653aa3725d88f8165a9de30610db903ef32f2765
Le Hong Dang <hdle@3D-LINUX-18.localdomain>
2025-11-07 10:00:59 +0100

    concac

--- Codes-TP2/Clean/main_udp_p2p.py
+++ Codes-TP2/Clean/main_udp_p2p.py
@@ -5,49 +5,49 @@ from log_tracer import *
 
 def experiment(buffer_size, bandwith , delay, app_data_rate, app_packet_size, app_start_time, app_duration_time, pcap_file, aggreg_res_file): 
      
-    nodes= ns.network.NodeContainer()
+    nodes= ns.NodeContainer()
     nodes.Create(2)
     pointToPoint = ns.PointToPointHelper() 
-    pointToPoint.SetQueue("ns3::DropTailQueue<Packet>", "MaxSize", ns. core.StringValue(str(buffer_size)+"p")) 
-    pointToPoint.SetDeviceAttribute("DataRate", ns.core.StringValue(str( bandwith)+"Kbps")) 
-    pointToPoint.SetChannelAttribute("Delay", ns.core.StringValue(str( delay)+"ms"))
+    pointToPoint.SetQueue("ns3::DropTailQueue<Packet>", "MaxSize", ns.StringValue(str(buffer_size)+"p")) 
+    pointToPoint.SetDeviceAttribute("DataRate", ns.StringValue(str( bandwith)+"Kbps")) 
+    pointToPoint.SetChannelAttribute("Delay", ns.StringValue(str( delay)+"ms"))
 
     devices = pointToPoint.Install(nodes)
 
-    stack = ns.internet.InternetStackHelper() 
+    stack = ns.InternetStackHelper() 
     stack.Install(nodes) 
-    address = ns.internet.Ipv4AddressHelper() 
+    address = ns.Ipv4AddressHelper() 
     address.SetBase("192.168.1.0", "255.255.255.0") 
     IPv4interfaces = address.Assign(devices)
 
-    traffic_control_helper = ns.network.TrafficControlHelper() 
+    traffic_control_helper = ns.TrafficControlHelper() 
     traffic_control_helper.Uninstall(devices)
 
     server_ip = IPv4interfaces.GetAddress(1) 
-    udpServer = ns.applications.PacketSinkHelper("ns3::UdpSocketFactory", ns.network.InetSocketAddress(server_ip, 9).ConvertTo()) 
+    udpServer = ns.PacketSinkHelper("ns3::UdpSocketFactory", ns.InetSocketAddress(server_ip, 9).ConvertTo()) 
     serverApps = udpServer.Install(nodes.Get(1)) 
-    serverApps.Start(ns.core.Seconds(0)) 
-    serverApps.Stop(ns.core.Seconds(200))
+    serverApps.Start(ns.Seconds(0)) 
+    serverApps.Stop(ns.Seconds(200))
 
     client_ip = IPv4interfaces.GetAddress(0) 
     clientApps = [] 
     echoClient = [] 
     for i in range(len(app_data_rate)): 
-        echoClient.append(ns.applications.OnOffHelper("ns3:: UdpSocketFactory", ns.network.InetSocketAddress(server_ip, 9).ConvertTo())) 
-        echoClient[i].SetAttribute("Local", ns.network.AddressValue(ns. network.InetSocketAddress(client_ip, 2025 + i).ConvertTo())) 
-        echoClient[i].SetAttribute("OnTime", ns.core.StringValue("ns3:: ConstantRandomVariable[Constant=1]")) 
-        echoClient[i].SetAttribute("OffTime", ns.core.StringValue("ns3:: ConstantRandomVariable[Constant=0]")) 
-        echoClient[i].SetAttribute("DataRate", ns.core.StringValue(str( app_data_rate[i]) + "kbps")) 
-        echoClient[i].SetAttribute("PacketSize", ns.core.UintegerValue( app_packet_size[i]))
+        echoClient.append(ns.OnOffHelper("ns3:: UdpSocketFactory", ns.InetSocketAddress(server_ip, 9).ConvertTo())) 
+        echoClient[i].SetAttribute("Local", ns.AddressValue(ns.InetSocketAddress(client_ip, 2025 + i).ConvertTo())) 
+        echoClient[i].SetAttribute("OnTime", ns.StringValue("ns3:: ConstantRandomVariable[Constant=1]")) 
+        echoClient[i].SetAttribute("OffTime", ns.StringValue("ns3:: ConstantRandomVariable[Constant=0]")) 
+        echoClient[i].SetAttribute("DataRate", ns.StringValue(str( app_data_rate[i]) + "kbps")) 
+        echoClient[i].SetAttribute("PacketSize", ns.UintegerValue( app_packet_size[i]))
         clientApps.append(echoClient[i].Install(nodes.Get(0))) 
-        clientApps[i].Start(ns.core.Seconds(app_start_time[i])) 
-        clientApps[i].Stop(ns.core.Seconds(app_start_time[i] + app_duration_time[i]))
+        clientApps[i].Start(ns.Seconds(app_start_time[i])) 
+        clientApps[i].Stop(ns.Seconds(app_start_time[i] + app_duration_time[i]))
 
-    ns.network.PacketMetadata.Enable()
+    ns.PacketMetadata.Enable()
     pointToPoint.EnablePcap(pcap_file, devices.Get(0), True)
-    ns.core.Simulator.Stop(ns.core.Seconds(200)) 
-    ns.core.Simulator.Run() 
-    ns.core.Simulator.Destroy()
+    ns.Simulator.Stop(ns.Seconds(200)) 
+    ns.Simulator.Run() 
+    ns.Simulator.Destroy()
 	# A compléter en TP2
     return
 	
@@ -72,11 +72,11 @@ def main(argv):
 
     Init_pkt_tracer_functions()
     sys.stdout = open(all_data_file+".txt", 'w')
-    #experiment (BufSize,Bandwidh,Delay,DataRates, PktSizes,AppStartTime,AppDurationTime, all_data_file, SumFileName)
+    experiment (BufSize,Bandwidh,Delay,DataRates, PktSizes,AppStartTime,AppDurationTime, all_data_file, SumFileName)
     sys.stdout.close()
     sys.stdout = sys.__stdout__
 
-    process_and_plot_metrics(all_data_file+".txt", 0.1)
+    #process_and_plot_metrics(all_data_file+".txt", 0.1)
 
 if __name__ == '__main__':
     sys.exit (main (sys.argv))
